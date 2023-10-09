05b342a48939154a4200925735d7d11017d14e68
unknown <rogerpantilyowger@gmail.com>
2023-10-09 15:47:24 +0800

    added cors middleware

---
+++ src/middleware.tsx
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+const allowedOrigins =
+    process.env.NODE_ENV === "development"
+        ? [
+              "http://localhost:3000",
+              "http://localhost:3001",
+              "http://localhost:3002",
+          ]
+        : ["https://trendy-haven-admin.vercel.app/"]
+
+export function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl
+
+    if (pathname.startsWith("/api")) {
+        const origin = request.headers.get("origin")
+
+        if (origin && !allowedOrigins.includes(origin)) {
+            return new NextResponse(null, {
+                status: 400,
+                statusText: "Bad Request",
+                headers: {
+                    "Content-Type": "text/plain",
+                },
+            })
+        }
+
+        return NextResponse.next()
+    }
+}
