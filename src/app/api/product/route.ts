f82f5457571cdab66773defc67da9c026843cdab
unknown <rogerpantilyowger@gmail.com>
2023-10-12 19:45:38 +0800

    added get products api function

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,5 +1,6 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { getServerSession } from "next-auth"
+import { headers } from "next/headers"
 
 import prisma from "@/lib/prismaDb"
 import { zodCustomError } from "@/lib/zodCustomError"
@@ -41,6 +42,41 @@ export async function POST(request: Request) {
 
         return NextResponse.json(
             { message: "Registration Error" },
+            { status: 500 }
+        )
+    }
+}
+
+export async function GET(request: NextRequest) {
+    try {
+        const { searchParams } = new URL(request.url)
+
+        const pageNumber = +(searchParams.get("pageNumber") ?? 1)
+        const pageSize = +(searchParams.get("pageSize") ?? 10)
+        const skip = (pageNumber - 1) * pageSize
+
+        const [totalCount, products] = await prisma.$transaction([
+            prisma.product.count(),
+            prisma.product.findMany({
+                skip,
+                take: pageSize,
+            }),
+        ])
+
+        const totalPages = Math.ceil(totalCount / pageSize)
+
+        const requestHeaders = new Headers(request.headers)
+        requestHeaders.set("X-Total-Pages", totalPages.toString())
+
+        return NextResponse.json(
+            { products, pageNumber, pageSize, totalCount },
+            { status: 200 }
+        )
+    } catch (error) {
+        console.log("Failed to fetch products: ", error)
+
+        return NextResponse.json(
+            { message: "Failed to fetch products" },
             { status: 500 }
         )
     }
