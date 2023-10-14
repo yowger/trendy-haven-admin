0e0d76d6cbc8cd35550d0953b196c66e1c5989e2
unknown <rogerpantilyowger@gmail.com>
2023-10-14 15:27:30 +0800

    refactored delete products api hook

--- src/hooks/api/useDeleteProducts.ts
+++ src/hooks/api/useDeleteProducts.ts
@@ -3,23 +3,25 @@ import type { AxiosError, AxiosResponse } from "axios"
 
 import axiosPublic from "@/lib/axios"
 import { PRODUCT_QUERY_KEY } from "@/constants/queryKeys"
-import type { ProductInput } from "@/schemas/productSchema"
-import type { Product } from "@/types/productTypes"
 
-const deleteProducts = async (productIds: string[]): Promise<any> => {
-    const response: AxiosResponse<any> = await axiosPublic.delete(
-        "/api/product",
-        {
+interface deleteProductsResponse {
+    message: string
+}
+
+const deleteProducts = async (
+    productIds: string[]
+): Promise<deleteProductsResponse> => {
+    const response: AxiosResponse<deleteProductsResponse> =
+        await axiosPublic.delete("/api/product", {
             data: { productIds },
-        }
-    )
+        })
     return response.data
 }
 
 const useDeleteProducts = () => {
     const queryClient = useQueryClient()
 
-    return useMutation<any, AxiosError, string[]>({
+    return useMutation<deleteProductsResponse, AxiosError, string[]>({
         mutationFn: deleteProducts,
         onSuccess: (product) =>
             queryClient.invalidateQueries({
