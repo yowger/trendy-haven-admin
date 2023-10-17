16f596fcb5dc3ac0175c5a7785772e0d9a4f8ecf
unknown <rogerpantilyowger@gmail.com>
2023-10-17 19:53:27 +0800

    modified sentence

--- src/app/(root)/store/create/page.tsx
+++ src/app/(root)/store/create/page.tsx
@@ -8,7 +8,7 @@ export default function page(): React.JSX.Element {
         <>
             <DashboardTitle
                 title="Create Your Store"
-                description="Set up your online store in minutes. Customize your store name, add a logo, and define product categories. Start selling globally with your unique online storefront."
+                description="Set up your online store to access all its features. Customize your store name, add a logo, and define product categories. Start selling globally with your unique online storefront."
             />
             <Separator />
             <div className="lg:max-w-2xl">
