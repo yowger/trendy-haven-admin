9a4ae418552d7d317c54d5d523033c97c14a38d9
unknown <rogerpantilyowger@gmail.com>
2023-10-21 15:51:39 +0800

    modified nav

--- src/data/navigationLinks.ts
+++ src/data/navigationLinks.ts
@@ -1,9 +1,12 @@
 export const navigationLinks = [
     { href: "/dashboard/store", label: "Store" },
     { href: "/dashboard/products", label: "Products" },
-    { href: "/dashboard/categories", label: "Categories" },
-    { href: "/dashboard/sizes", label: "Sizes" },
-    { href: "/dashboard/colors", label: "Colors" },
+    /* 
+        for admin
+        { href: "/dashboard/categories", label: "Categories" },
+        { href: "/dashboard/sizes", label: "Sizes" },
+        { href: "/dashboard/colors", label: "Colors" },
+    */
     { href: "/dashboard/orders", label: "Orders" },
     { href: "/dashboard/settings", label: "Settings" },
 ]
