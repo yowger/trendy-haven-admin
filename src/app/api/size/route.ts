5f05d4f01e889e86b913c47462cf951739f1c975
unknown <rogerpantilyowger@gmail.com>
2023-10-21 23:14:45 +0800

    added get sizeendpoint

---
+++ src/app/api/size/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server"
+
+import prisma from "@/lib/prismaDb"
+
+export async function GET() {
+    try {
+        const sizes = await prisma.size.findMany({})
+
+        return NextResponse.json({ sizes }, { status: 200 })
+    } catch (error) {
+        console.log("Failed to fetch sizes: ", error)
+
+        return NextResponse.json({ message: "Server error" }, { status: 500 })
+    }
+}
