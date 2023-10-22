676781a996a410256bf34f366a36bfddfe97f13b
unknown <rogerpantilyowger@gmail.com>
2023-10-22 22:39:54 +0800

    modified create products form, now accepts variants

--- src/app/(dashboard)/dashboard/products/components/CreateProductForm.tsx
+++ src/app/(dashboard)/dashboard/products/components/CreateProductForm.tsx
@@ -1,23 +1,27 @@
 "use client"
 
 import { useEffect } from "react"
-import { Loader2 } from "lucide-react"
+import { Loader2, Trash } from "lucide-react"
 import {
     Control,
     Controller,
     FieldErrors,
     FieldPath,
+    useFieldArray,
     useForm,
 } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 
 import useCreateProduct from "@/hooks/api/useCreateProduct"
+import useGetCategories from "@/hooks/api/useGetCategories"
+import useGetSizes from "@/hooks/api/useGetSizes"
+import useGetColors from "@/hooks/api/useGetColors"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-
 import {
     Form,
     FormControl,
+    FormDescription,
     FormField,
     FormItem,
     FormLabel,
@@ -27,20 +31,66 @@ import { ProductInputSchema } from "@/schemas/productSchema"
 import type { ProductInput } from "@/schemas/productSchema"
 import { handleDecimalsOnValue } from "@/lib/handlePriceChange"
 import { useToast } from "@/components/ui/use-toast"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Checkbox } from "@/components/ui/checkbox"
+import { cn } from "@/lib/utils"
+import { Label } from "@/components/ui/label"
 
 export default function CreateProductForm(): JSX.Element {
     const { toast } = useToast()
+
+    const {
+        isLoading: categoriesLoading,
+        isError: categoriesError,
+        error: categoriesErrorMessage,
+        data: categoriesData,
+        isFetching: categoriesIsFetching,
+        isPreviousData: categoriesIsPreviousData,
+    } = useGetCategories()
+
+    const {
+        isLoading: sizesLoading,
+        isError: sizesError,
+        error: sizesErrorMessage,
+        data: sizesData,
+        isFetching: sizesIsFetching,
+        isPreviousData: sizesIsPreviousData,
+    } = useGetSizes()
+
+    const {
+        isLoading: colorsLoading,
+        isError: colorsError,
+        error: colorsErrorMessage,
+        data: colorsData,
+        isFetching: colorsIsFetching,
+        isPreviousData: colorsIsPreviousData,
+    } = useGetColors()
 
     const { data, mutate, isLoading, isSuccess, error } = useCreateProduct()
 
     const form = useForm<ProductInput>({
         resolver: zodResolver(ProductInputSchema),
+        defaultValues: {
+            stocks: [{ size: "", color: "", quantity: 0, price: 0 }],
+        },
     })
 
     const onSubmit = (data: ProductInput): void => {
-        const { name, price } = data
+        // const { name, price, category } = data
+        // form.reset({
+        //     name: "",
+        //     price: 0,
+        //     quantity: 0,
+        // })
+        console.log("form data: ", data)
 
-        mutate({ name, price })
+        // mutate({ name, price })
     }
 
     useEffect(() => {
@@ -52,10 +102,7 @@ export default function CreateProductForm(): JSX.Element {
 
     useEffect(() => {
         if (isSuccess) {
-            form.reset({
-                name: "",
-                price: "",
-            })
+            form.reset({})
 
             toast({
                 duration: 2000,
@@ -63,6 +110,20 @@ export default function CreateProductForm(): JSX.Element {
             })
         }
     }, [isSuccess, form, toast])
+
+    const {
+        fields: variantFields,
+        append,
+        remove,
+    } = useFieldArray({
+        control: form.control,
+        name: "stocks",
+    })
+
+    const onVariantAppend = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        append({ size: "", color: "", quantity: 0, price: 0 })
+    }
 
     return (
         <Form {...form}>
@@ -73,7 +134,7 @@ export default function CreateProductForm(): JSX.Element {
                     render={({ field }) => {
                         return (
                             <FormItem>
-                                <FormLabel>Name</FormLabel>
+                                <FormLabel>Product name</FormLabel>
                                 <FormControl>
                                     <Input {...field} />
                                 </FormControl>
@@ -83,26 +144,195 @@ export default function CreateProductForm(): JSX.Element {
                     }}
                 />
 
-                <ControllerPlus
-                    transform={{
-                        input: (value) => String(value),
-                        output: (event) => {
-                            return handleDecimalsOnValue(event.target.value)
-                        },
-                    }}
+                <FormField
                     control={form.control}
-                    name="price"
-                    defaultValue=""
-                    errors={form.formState.errors}
+                    name="category"
+                    render={({ field }) => (
+                        <FormItem className="">
+                            <FormLabel>Category</FormLabel>
+                            <Select
+                                onValueChange={field.onChange}
+                                defaultValue={field.value}
+                            >
+                                <FormControl>
+                                    <SelectTrigger
+                                        className={cn("w-full md:w-72")}
+                                    >
+                                        <SelectValue placeholder="Select category" />
+                                    </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                    {categoriesData?.categories?.map(
+                                        (category) => {
+                                            return (
+                                                <SelectItem
+                                                    key={category.id}
+                                                    value={category.id}
+                                                >
+                                                    <span className="capitalize">
+                                                        {category.name}
+                                                    </span>
+                                                </SelectItem>
+                                            )
+                                        }
+                                    )}
+                                </SelectContent>
+                            </Select>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
+
+                <div className="flex justify-between">
+                    <p>Variants</p>
+                    <Button
+                        onClick={(event) => onVariantAppend(event)}
+                        variant="outline"
+                    >
+                        Add Variant
+                    </Button>
+                </div>
+
+                {variantFields.map((field, index) => {
+                    return (
+                        <div key={field.id} className="md:flex gap-x-4 space-y-8">
+                            <div className="grid grid-cols-2 md:grid-cols-4 gap-x-4 gap-y-8">
+                                <FormField
+                                    name={`stocks.${index}.size`}
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Size</FormLabel>
+                                            <Select
+                                                onValueChange={field.onChange}
+                                                defaultValue={field.value}
+                                            >
+                                                <FormControl>
+                                                    <SelectTrigger>
+                                                        <SelectValue placeholder="Size" />
+                                                    </SelectTrigger>
+                                                </FormControl>
+                                                <SelectContent>
+                                                    {sizesData?.sizes?.map(
+                                                        (size) => {
+                                                            return (
+                                                                <SelectItem
+                                                                    key={
+                                                                        size.id
+                                                                    }
+                                                                    value={
+                                                                        size.id
+                                                                    }
+                                                                >
+                                                                    <span className="uppercase">
+                                                                        {
+                                                                            size.name
+                                                                        }
+                                                                    </span>
+                                                                </SelectItem>
+                                                            )
+                                                        }
+                                                    )}
+                                                </SelectContent>
+                                            </Select>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+
+                                <FormField
+                                    name={`stocks.${index}.color`}
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Color</FormLabel>
+                                            <Select
+                                                onValueChange={field.onChange}
+                                                defaultValue={field.value}
+                                            >
+                                                <FormControl>
+                                                    <SelectTrigger>
+                                                        <SelectValue placeholder="Color" />
+                                                    </SelectTrigger>
+                                                </FormControl>
+                                                <SelectContent>
+                                                    {colorsData?.colors?.map(
+                                                        (color) => {
+                                                            return (
+                                                                <SelectItem
+                                                                    key={
+                                                                        color.id
+                                                                    }
+                                                                    value={
+                                                                        color.id
+                                                                    }
+                                                                >
+                                                                    <span className="capitalize">
+                                                                        {
+                                                                            color.name
+                                                                        }
+                                                                    </span>
+                                                                </SelectItem>
+                                                            )
+                                                        }
+                                                    )}
+                                                </SelectContent>
+                                            </Select>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+
+                                <ControllerPlus
+                                    transform={{
+                                        input: (value) => String(value),
+                                        output: (event) => {
+                                            return event.target.value.replace(
+                                                /[^0-9]/g,
+                                                ""
+                                            )
+                                        },
+                                    }}
+                                    control={form.control}
+                                    name={`stocks.${index}.quantity`}
+                                    label="Quantity"
+                                    defaultValue=""
+                                    errors={form.formState.errors}
+                                />
+
+                                <ControllerPlus
+                                    transform={{
+                                        input: (value) => String(value),
+                                        output: (event) => {
+                                            return handleDecimalsOnValue(
+                                                event.target.value
+                                            )
+                                        },
+                                    }}
+                                    control={form.control}
+                                    name={`stocks.${index}.price`}
+                                    label="Price"
+                                    defaultValue=""
+                                    errors={form.formState.errors}
+                                />
+                            </div>
+
+                            <div className="self-end">
+                                <Button
+                                    size="icon"
+                                    className="justify-self-end"
+                                >
+                                    <Trash className="h-4 w-4" />
+                                </Button>
+                            </div>
+                        </div>
+                    )
+                })}
 
                 {error && (
                     <FormMessage>
-                        Failed to add product, please check your internet
-                        connection and try again
+                        {`Oops! Something went wrong on our end. Please try again
+                        later.`}
                     </FormMessage>
                 )}
-
                 <Button
                     onClick={form.handleSubmit(onSubmit)}
                     disabled={isLoading}
@@ -123,6 +353,7 @@ const ControllerPlus = <TInput extends string, TOutput>({
     name,
     defaultValue,
     errors,
+    label,
 }: {
     transform: {
         input: (value: TOutput) => TInput
@@ -132,6 +363,7 @@ const ControllerPlus = <TInput extends string, TOutput>({
     control: Control<ProductInput>
     defaultValue?: any
     errors: FieldErrors
+    label?: React.ReactNode
 }): JSX.Element => {
     return (
         <Controller
@@ -142,7 +374,7 @@ const ControllerPlus = <TInput extends string, TOutput>({
                 const error = errors[name]
                 return (
                     <FormItem>
-                        <FormLabel>Price</FormLabel>
+                        {label && <FormLabel>{label}</FormLabel>}
                         <FormControl>
                             <Input
                                 {...field}
--- src/app/(dashboard)/dashboard/products/create/page.tsx
+++ src/app/(dashboard)/dashboard/products/create/page.tsx
@@ -1,11 +1,30 @@
-import DashboardTitle from "@/components/dashboard/DashboardTitle"
+import { dehydrate } from "@tanstack/react-query"
+
+import getQueryClient from "@/lib/getQueryClient"
+import { getCategories } from "@/hooks/api/useGetCategories"
+import { getSizes } from "@/hooks/api/useGetSizes"
+import { getColors } from "@/hooks/api/useGetColors"
+import {
+    CATEGORY_QUERY_KEY,
+    COLOR_QUERY_KEY,
+    SIZE_QUERY_KEY,
+} from "@/constants/queryKeys"
+import HydrateClient from "@/components/tanstack/HydrateClient"
 import { Separator } from "@/components/ui/separator"
+import DashboardTitle from "@/components/dashboard/DashboardTitle"
 
 import CreateProductForm from "../components/CreateProductForm"
 
-export default function page() {
+export default async function page() {
+    const queryClient = getQueryClient()
+
+    await queryClient.prefetchQuery([CATEGORY_QUERY_KEY], () => getCategories())
+    await queryClient.prefetchQuery([SIZE_QUERY_KEY], () => getSizes())
+    await queryClient.prefetchQuery([COLOR_QUERY_KEY], () => getColors())
+
+    const dehydratedState = dehydrate(queryClient)
     return (
-        <>
+        <HydrateClient state={dehydratedState}>
             <DashboardTitle
                 title="Create a New Product"
                 description=" Enter the details of your new product below."
@@ -14,6 +33,6 @@ export default function page() {
             <div className="lg:max-w-2xl">
                 <CreateProductForm />
             </div>
-        </>
+        </HydrateClient>
     )
 }
