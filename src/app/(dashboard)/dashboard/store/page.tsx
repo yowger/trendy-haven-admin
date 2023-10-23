90831117aae1e89a5d9c5b3be90febce1c5f28d7
unknown <rogerpantilyowger@gmail.com>
2023-10-23 21:00:00 +0800

    added cards and placeholder for store dashboard

--- src/app/(dashboard)/dashboard/store/page.tsx
+++ src/app/(dashboard)/dashboard/store/page.tsx
@@ -1,5 +1,14 @@
+import { DollarSign, MessageSquare, TrendingUp, Users } from "lucide-react"
+
 import DashboardTitle from "@/components/dashboard/DashboardTitle"
 import { Separator } from "@/components/ui/separator"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 export default function Store(): JSX.Element {
     return (
@@ -11,6 +20,66 @@ export default function Store(): JSX.Element {
                 />
             </div>
             <Separator />
+
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                            Total Revenue
+                        </CardTitle>
+                        <TrendingUp className="w-4 h-4 shrink-0 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">$45,231.89</div>
+                        <p className="text-xs text-muted-foreground">
+                            +$10,500 from last month
+                        </p>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                            Total Sales
+                        </CardTitle>
+                        <DollarSign className="w-4 h-4 shrink-0 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">12,234</div>
+                        <p className="text-xs text-muted-foreground">
+                            +300 from last month
+                        </p>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                            Total Products
+                        </CardTitle>
+                        <Users className="w-4 h-4 shrink-0 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">2350</div>
+                        <p className="text-xs text-muted-foreground">
+                            +250 from last month
+                        </p>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                            Product Reviews
+                        </CardTitle>
+                        <MessageSquare className="w-4 h-4 shrink-0 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">2500</div>
+                        <p className="text-xs text-muted-foreground">
+                            +201 since last week
+                        </p>
+                    </CardContent>
+                </Card>
+            </div>
         </>
     )
 }
